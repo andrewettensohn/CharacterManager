6a9e2e6539cddf418f9ea410123c62704be895de
Andrew Ettensohn <andrewettensohn@gmail.com>
2021-04-25 23:50:59 -0400

    Quest model

---
+++ CharacterManager.Models/Quest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterManager.Models
+{
+    public class Quest
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsMainQuest { get; set; }
+
+        public bool IsComplete { get; set; }
+
+        public string BriefDescription { get; set; }
+
+        public string FullDescription { get; set; }
+    }
+}
