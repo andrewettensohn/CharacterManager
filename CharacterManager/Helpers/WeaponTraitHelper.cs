9674f69c40f12a334cf44dfaa8ba365d6bf5d9b7
Andrew Ettensohn <andrewettensohn@gmail.com>
2021-05-26 13:58:28 -0400

    Weapon trait info

--- CharacterManager/Helpers/WeaponTraitHelper.cs
+++ CharacterManager/Helpers/WeaponTraitHelper.cs
@@ -1,20 +1,26 @@
-﻿using System;
+﻿using CharacterManager.UserMessages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace CharacterManager.Helpers
 {
     public class WeaponTraitHelper
     {
-        public static Dictionary<string, string> GetWeaponTraitUserMessages(List<string> traits)
+        public static Dictionary<string, string> GetWeaponTraitUserMessages(string traits)
         {
             Dictionary<string, string> messageDictionary = new Dictionary<string, string>();
 
-            foreach (string trait in traits)
-            {
+            IEnumerable<FieldInfo> messageProperties = typeof(WeaponTraitUserMessages).GetFields(BindingFlags.Static | BindingFlags.Public).Where(x => traits.Contains(x.Name));
 
+            foreach (FieldInfo messageProperty in messageProperties)
+            {
+                messageDictionary.Add(messageProperty.Name, messageProperty.GetValue(messageProperty).ToString());
             }
+
+            return messageDictionary;
         }
     }
 }
